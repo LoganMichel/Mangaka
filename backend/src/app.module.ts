d18e1a406f5ba45471605e6291d9aaf12d2bf016
Logan MICHEL <michelogan@gmail.com>
2025-02-22 16:06:50 +0100

    ADD backend sqlite3 + typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // À désactiver en production
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
